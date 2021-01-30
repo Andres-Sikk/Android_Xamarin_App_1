367856701966c9f35f3467ebaccadda868f5b5c7
Andres Sikk <58027474+Andres-Sikk@users.noreply.github.com>
2021-01-30 21:15:06 +0200

    add clickcounter and calculator activity

---
+++ Android_Xamarin_App_1/CalculatorActivity1.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using System;
+
+namespace Android_Xamarin_App_1
+{
+    [Activity(Label = "CalculatorActivity1")]
+    public class CalculatorActivity1 : Activity
+    {
+        EditText _calcNumber1;
+        EditText _calcNumber2;
+        TextView _calcAnswer;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.calculator_layout);
+            _calcNumber1 = FindViewById<EditText>(Resource.Id.calcNumber1);
+            _calcNumber2 = FindViewById<EditText>(Resource.Id.calcNumber2);
+            _calcAnswer = FindViewById<TextView>(Resource.Id.calcAnswer);
+            var calcButton = FindViewById<Button>(Resource.Id.calcButton);
+            calcButton.Click += CalcAddNumbers;
+        }
+        private void CalcAddNumbers(object sender, EventArgs e)
+        {
+            _calcAnswer.Text = (int.Parse(_calcNumber1.Text) + int.Parse(_calcNumber2.Text)).ToString();
+        }
+    }
+}
---
+++ Android_Xamarin_App_1/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Widget;
+
+namespace Android_Xamarin_App_1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            int clickCounter1 = 0;
+            var textView = FindViewById<TextView>(Resource.Id.textView1);
+            var button = FindViewById<Button>(Resource.Id.button1);
+            var calculatorButton = FindViewById<Button>(Resource.Id.calculatorButton);
+            button.Click += delegate
+            {
+                clickCounter1++;
+                textView.Text = clickCounter1.ToString();
+            };
+            calculatorButton.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(CalculatorActivity1));
+                StartActivity(intent);
+            };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
